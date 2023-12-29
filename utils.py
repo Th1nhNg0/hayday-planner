ac1f3b43cd3f8c996efbf76a9f744d56b9dd02cf
Th1nhNg0 <ngophuthinh2001@gmail.com>
2023-12-29 15:10:42 +0700

    Refactor find_needed_for function to correctly handle nested needs

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import json
+
+with open('goods.json', 'r') as f:
+    rows = json.load(f)
+
 def find_by_name(name):
     for row in rows:
         if row['Name'].lower() == name.lower():
@@ -9,14 +14,14 @@ def find_needed_for(name,count=1):
     if row is None:
         raise Exception(f'Could not find {name}')
     result = row.copy()
-    result['needs'] = []
-    result['count'] = count
-    for need in row['Needs']:
+    needs = result['Needs'].copy()
+    result['Needs'] = []
+    result['Count'] = count
+    for need in needs:
         for key, value in need.items():
             if (key==name):
                 continue
-            result['needs'].append(find_needed_for(key, value*count))
-    del result['Needs']
+            result['Needs'].append(find_needed_for(key, value*count))
     return result
 
 
